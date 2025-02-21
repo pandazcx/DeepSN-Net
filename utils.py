831e61374161abf75476dcd9d817dac13bcfd8e3
Chenxiao Zhang <zxcc12103637@gmail.com>
2025-02-21 09:30:09 +0800

    Add files via upload
    Supplemented file utils.py

--- utils.py
+++ utils.py
@@ -617,3 +617,96 @@ def to_y_channel(img):
         img = bgr2ycbcr(img, y_only=True)
         img = img[..., None]
     return img * 255.
+
+
+def image_align(deblurred, gt):
+  # this function is based on kohler evaluation code
+  z = deblurred
+  c = np.ones_like(z)
+  x = gt
+
+  zs = (np.sum(x * z) / np.sum(z * z)) * z # simple intensity matching
+
+  warp_mode = cv2.MOTION_HOMOGRAPHY
+  warp_matrix = np.eye(3, 3, dtype=np.float32)
+
+  # Specify the number of iterations.
+  number_of_iterations = 100
+
+  termination_eps = 0
+
+  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+              number_of_iterations, termination_eps)
+
+  # Run the ECC algorithm. The results are stored in warp_matrix.
+  try:
+    (cc, warp_matrix) = cv2.findTransformECC(cv2.cvtColor(x, cv2.COLOR_RGB2GRAY), cv2.cvtColor(zs, cv2.COLOR_RGB2GRAY), warp_matrix, warp_mode, criteria, inputMask=None, gaussFiltSize=5)
+  except (cv2.error):
+      print("wrong!")
+  target_shape = x.shape
+  shift = warp_matrix
+
+  zr = cv2.warpPerspective(
+    zs,
+    warp_matrix,
+    (target_shape[1], target_shape[0]),
+    flags=cv2.INTER_CUBIC+ cv2.WARP_INVERSE_MAP,
+    borderMode=cv2.BORDER_REFLECT)
+
+  cr = cv2.warpPerspective(np.ones_like(zs, dtype='float32'),warp_matrix,(target_shape[1], target_shape[0]),flags=cv2.INTER_NEAREST+ cv2.WARP_INVERSE_MAP,
+    borderMode=cv2.BORDER_CONSTANT,borderValue=0)
+
+  zr = zr * cr
+  xr = x * cr
+
+  return zr, xr, cr, shift
+
+def realblur_psnr(image_true, image_test, image_mask, data_range=None):
+  # this function is based on skimage.metrics.peak_signal_noise_ratio
+  err = np.sum((image_true - image_test) ** 2, dtype=np.float64) / np.sum(image_mask)
+  return 10 * np.log10((data_range ** 2) / err)
+
+
+def realblur_ssim(tar_img, prd_img, cr1):
+    ssim_pre, ssim_map = structural_similarity(tar_img, prd_img, channel_axis=-1, gaussian_weights=True, use_sample_covariance=False, data_range = 1.0, full=True)
+    ssim_map = ssim_map * cr1
+    r = int(3.5 * 1.5 + 0.5)  # radius as in ndimage
+    win_size = 2 * r + 1
+    pad = (win_size - 1) // 2
+    ssim = ssim_map[pad:-pad,pad:-pad,:]
+    crop_cr1 = cr1[pad:-pad,pad:-pad,:]
+    ssim = ssim.sum(axis=0).sum(axis=0)/crop_cr1.sum(axis=0).sum(axis=0)
+    ssim = np.mean(ssim)
+    return ssim
+
+def realblur_cal(prd_img,tar_img):
+    # tensor, CHW, 0-1
+    prd_img = np.transpose(prd_img.cpu().numpy(), (1, 2, 0))
+    tar_img = np.transpose(tar_img.cpu().numpy(), (1, 2, 0))
+    prd_img, tar_img, cr1, shift = image_align(prd_img, tar_img)
+    PSNR = realblur_psnr(tar_img, prd_img, cr1, data_range=1)
+    SSIM = realblur_ssim(tar_img, prd_img, cr1)
+    return PSNR, SSIM
+
+
+# def ycbcr_ssim(img1, img2):
+#     if img1.ndim == 3:
+#         img1 = rgb2ycbcr(img1)[:, :, 0]
+#     if img2.ndim == 3:
+#         img2 = rgb2ycbcr(img2)[:, :, 0]
+#
+#     ssim_mean, _ = structural_similarity(img1, img2, full=True)
+#     return ssim_mean
+#
+#
+# def ycbcr_psnr(img1, img2):
+#     if img1.ndim == 3:
+#         img1 = rgb2ycbcr(img1)[:, :, 0]
+#     if img2.ndim == 3:
+#         img2 = rgb2ycbcr(img2)[:, :, 0]
+#
+#     imdff = img1.astype(np.float64) - img2.astype(np.float64)
+#     rmse = np.sqrt(np.mean(imdff ** 2))
+#     psnr = 20 * np.log10(255 / rmse)
+#
+#     return psnr
